da4ca1bee11032bbcb68afab89b85fc6f9456472
Zehir <zehir@zorim.fr>
2024-04-28 15:11:09 +0000

    error handling

--- src/bundler.ts
+++ src/bundler.ts
@@ -1,26 +1,40 @@
 import * as core from '@actions/core'
+import type { Bundle } from '@deconz-community/ddf-bundler'
 import { buildFromFiles } from '@deconz-community/ddf-bundler'
 import type { InputsParams } from './input'
 import type { Sources } from './source'
+import { handleError } from './errors'
 
-export async function runBundler(params: InputsParams, sources: Sources) {
+export async function runBundler(params: InputsParams, sources: Sources): Promise<ReturnType<typeof Bundle>[]> {
   if (!params.bundler.enabled)
     throw new Error('Can\'t run bundler because he is not enabled')
 
-  sources.getDDFPaths().forEach(async (ddfPath) => {
+  const bundles: ReturnType<typeof Bundle>[] = []
+
+  Promise.all(sources.getDDFPaths().map(async (ddfPath) => {
     core.info(`Found DDF ${ddfPath}`)
 
-    const bundle = await buildFromFiles(
-      `file://${params.source.path.generic}`,
-      `file://${ddfPath}`,
-      async (path) => {
-        const newPath = path.replace('file://', '')
-        core.info(`Need file ${newPath}`)
-        return await sources.getFile(newPath)
-      },
-    )
+    try {
+      const bundle = await buildFromFiles(
+        `file://${params.source.path.generic}`,
+        `file://${ddfPath}`,
+        async (path) => {
+          const newPath = path.replace('file://', '')
+          core.info(`Need file ${newPath}`)
+          return await sources.getFile(newPath)
+        },
+      )
 
-    core.info(`Bundle ${ddfPath} created`)
-    core.info(JSON.stringify(bundle.data.desc, null, 2))
-  })
+      bundles.push(bundle)
+
+      core.info(`Bundle ${ddfPath} created`)
+    }
+    catch (err) {
+      core.error(`Error while creating bundle ${ddfPath}`)
+      const file = await sources.getFile(ddfPath)
+      handleError(err, ddfPath, await file.text())
+    }
+  }))
+
+  return bundles
 }
