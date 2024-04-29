4af27191540f01d25074650273c449ffef3faa20
Zehir <zehir@zorim.fr>
2024-04-29 19:42:21 +0000

    test upload

--- src/uploader.ts
+++ src/uploader.ts
@@ -3,6 +3,12 @@ import * as core from '@actions/core'
 import { authentication, createDirectus, rest, serverHealth, staticToken } from '@directus/sdk'
 import type { InputsParams } from './input'
 
+type UploadResponse = Record<string, {
+  success: boolean
+  createdId?: string
+  message?: string
+}>
+
 export async function runUploader(params: InputsParams, memoryBundles: ReturnType<typeof Bundle>[]) {
   const { upload } = params
   if (!upload.enabled)
@@ -27,7 +33,34 @@ export async function runUploader(params: InputsParams, memoryBundles: ReturnTyp
 
   const health = await client.request(serverHealth())
 
+  if (health.status !== 'ok')
+    throw new Error(`Server health is not ok: ${health.status}`)
+
   core.info(`health.status=${health.status}`)
+
+  const bulkSize = 10
+
+  for (let i = 0; i < bundles.length; i += bulkSize) {
+    const group = bundles.slice(i, i + bulkSize)
+    const formData = new FormData()
+
+    for (let j = 0; j < group.length; j++) {
+      const bundle = group[j]
+      formData.append(`bundle-${i + j + 1}`, bundle)
+    }
+
+    const result = await client.request<{ result: UploadResponse }>(() => {
+      return {
+        method: 'POST',
+        path: '/bundle/upload',
+        body: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      }
+    })
+
+    core.info(`Uploaded ${group.length} bundles`)
+    core.info(JSON.stringify(result.result, null, 2))
+  }
 
   core.info('Uploading bundles')
 
