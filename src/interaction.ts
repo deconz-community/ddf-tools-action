d52ac2c9c8498660740ccb92c3130412eca536f3
Zehir <zehir@zorim.fr>
2024-05-02 20:03:22 +0000

    test template parsing

--- src/interaction.ts
+++ src/interaction.ts
@@ -1,11 +1,31 @@
-import type { Source } from '@deconz-community/ddf-bundler'
+import path from 'node:path'
+import fs from 'node:fs/promises'
 import type { Context } from '@actions/github/lib/context'
 import { Octokit } from '@octokit/action'
 import type { PullRequestEvent } from '@octokit/webhooks-types'
-import * as core from '@actions/core'
+import { Liquid } from 'liquidjs'
 import type { MemoryBundle } from './bundler'
-import type { BundlerSourceMetadata, FileStatus } from './source'
 import type { InputsParams } from './input'
+
+interface BundleInfo {
+  path: string
+}
+
+interface Templates {
+  'modified-bundles': {
+    pull_request: PullRequestEvent['pull_request']
+    added_bundles: BundleInfo[]
+    modified_bundles: BundleInfo[]
+    deleted_bundles: BundleInfo[]
+    artifact: {
+      enabled: true
+      url: string
+      retention_days: number
+    } | {
+      enabled: false
+    }
+  }
+}
 
 export async function getExistingCommentsPR(
   context: Context,
@@ -27,6 +47,12 @@ export async function getExistingCommentsPR(
     })
 }
 
+export async function parseTemplate<TemplateName extends keyof Templates>(name: TemplateName, data: Templates[TemplateName]) {
+  const template = await fs.readFile(path.join(__dirname, `../templates/${name}.md`), 'utf-8')
+  const engine = new Liquid()
+  return await engine.parseAndRender(template, data)
+}
+
 export async function updateModifiedBundleInteraction(
   params: InputsParams,
   context: Context,
@@ -44,11 +70,17 @@ export async function updateModifiedBundleInteraction(
     return comment.body?.startsWith('<!-- DDF-TOOLS-ACTION/modified-bundles -->')
   })
 
-  const body = `<!-- DDF-TOOLS-ACTION/modified-bundles -->\n${bundle
-    .map((memoryBundle) => {
-      return `* ${memoryBundle.path} is ${memoryBundle.status}`
-    })
-    .join('\n')}`
+  const body = await parseTemplate('modified-bundles', {
+    added_bundles: [{ path: 'foo' }],
+    modified_bundles: [{ path: 'bar' }],
+    deleted_bundles: [{ path: 'baz' }],
+    pull_request: payload.pull_request,
+    artifact: {
+      enabled: true,
+      url: 'https://example.com',
+      retention_days: 5,
+    },
+  })
 
   if (existingComment !== undefined) {
     await octokit.rest.issues.updateComment({
